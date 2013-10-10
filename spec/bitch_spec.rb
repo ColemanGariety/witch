1455a1838bfda9bd056821390964a7ba1b6ca278
akonwi <akonwi@gmail.com>
2013-10-10 15:27:58 -0500

    Changing the silence! method to toggle silentness and adding a silent? method. Also bumping version to 0.0.3

--- spec/bitch_spec.rb
+++ spec/bitch_spec.rb
@@ -5,10 +5,23 @@ describe Bitch do
     it "should send a message" do
       Bitch.yell("message").should_not be_false
     end
-    
+
     it "should not send a message" do
       Bitch.silence!
-      Bitch.yell("message").should_not be_true
+      Bitch.yell("silent message").should_not be_true
+    end
+
+    it "should not send a message after toggling silence" do
+      Bitch.silence!
+      Bitch.yell('message').should_not be_false
+    end
+  end
+
+  describe ".silent?" do
+    it "should let you know if Bitch is silent" do
+      Bitch.silent?.should be_false
+      Bitch.silence!
+      Bitch.silent?.should be_true
     end
   end
 end
